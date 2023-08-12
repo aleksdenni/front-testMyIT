822f8c612e86f70794b53d791afceedd1025172e
Maksym <maksymsydorovych@gmail.com>
2023-08-12 21:53:14 +0200

    setup app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { store } from "./app/store";
+import {Provider} from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(
+    <BrowserRouter>
+        <Provider store={store}> <App/></Provider>
+    </BrowserRouter>
+);
