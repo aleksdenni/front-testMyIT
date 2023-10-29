be0c325021a86b76e05d1f25bb7d5129d0c6cd82
Maksym <maksymsydorovych@gmail.com>
2023-10-29 17:38:24 +0100

    deploy on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import * as path from 'path'
+import * as path from "path";
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
   server: {
     open: true,
   },
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-})
+});
